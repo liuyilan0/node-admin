cc793c5f3b95c289af7644c01860ec53be11bc85
liuyilan <2995293916@qq.com>
2020-03-12 23:42:02 +0800

    电子书解析

--- router/book.js
+++ router/book.js
@@ -3,6 +3,7 @@ const Result = require('../models/result')  // 接口处理
 const multer = require('multer')   // 文件上传
 const { UPLOAD_PATH } = require('../utils/constant')
 const Book = require('../models/book')
+const boom = require('boom')
 
 const router = express.Router()
 
@@ -19,8 +20,11 @@ router.post('/upload', multer({ dest: `${UPLOAD_PATH}/book` }).single('file'),
             new Result('上传电子书失败').fail(res)
         } else {
             const book = new Book(req.file)
-            console.log(book)
-            new Result('上传电子书成功').success(res)
+            book.parse().then(book => {
+                new Result(book, '上传电子书成功').success(res)
+            }).catch(err => {
+                next(boom.badImplementation(err))
+            })
         }
     })
 
